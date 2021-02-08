b8a906ec329189154ee360c1dd8ef8dd0712e73e
MaelDelaby <maeldelabouy@yahoo.fr>
2021-02-08 14:56:55 +0100

    add reactJS project

---
+++ server/model/classAge.js
@@ -0,0 +1,9 @@
+var col = {
+    reg,//Region
+    cl_age90,//Classe age
+    jour,//Date de notification
+    hosp,//Nombre de personnes actuellement hospitalisées
+    rea,//Nombre de personnes actuellement en réanimation ou soins intensifs
+    rad,//Nombre cumulé de personnes retournées à domicile
+    dc//Nombre cumulé de personnes décédées
+}
