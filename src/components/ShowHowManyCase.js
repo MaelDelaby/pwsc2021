b33b18c016edbd6dcf3aa6e552737840124ccd6c
MaelDelaby <maeldelabouy@yahoo.fr>
2021-02-22 14:23:55 +0100

    [front] code plus propre sans depreciate #8

--- src/components/ShowHowManyCase.js
+++ src/components/ShowHowManyCase.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { makeStyles } from '@material-ui/core/styles';
 
@@ -20,19 +20,25 @@ export default function ShowHowManyCase() {
     const urlForFrance = "https://coronavirusapi-france.now.sh/FranceLiveGlobalData";
     const timeoutToRefesh = 1000;
 
-    function refresh(){
-        axios.get(urlForFrance)
-        .then(data => {
-                setNbOfCase(data.data.FranceGlobalLiveData[0].casConfirmes);
-            }
-        )
-        .catch(
-            err=>console.log(err)
-        )
+    async function refresh(){
+        try {
+            const data = await axios.get(urlForFrance);
+            return data.data.FranceGlobalLiveData[0].gueris + data.data.FranceGlobalLiveData[0].deces;
+        } catch (error) {
+            console.log(error);
+        }
     }
 
-    setInterval(refresh, timeoutToRefesh);
-
+    useEffect(() => {
+        const fetchData = async () => {
+            setNbOfCase(await refresh());
+        };
+        fetchData();
+        let interval = setInterval(async () => fetchData(), timeoutToRefesh);
+        return () => {
+            clearInterval(interval);
+        };
+    }, []);
 
 
     return (
