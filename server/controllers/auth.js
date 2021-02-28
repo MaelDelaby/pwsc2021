6ad9a1631825b4996cd2daff896ff447a35c0a4a
TianyangCHEN <tianyang.chen@etu.univ-cotedazur.fr>
2021-02-28 17:56:43 +0100

    [back] add sign In #16

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,8 +1,7 @@
 const User=require('../models/userModel');
 
 
-
-   const signUp=async(req,res)=>{ 
+const signUp=async(req,res)=>{
       
         let bigggestUser = await User.find().sort({id:-1}).skip(0).limit(1).exec();
         let biggestID = 0;
@@ -29,5 +28,45 @@ const User=require('../models/userModel');
             return "User added"
         });
     
-   }
-   module.exports={signUp}
+}
+
+
+
+const signIn = async(req,res)=>{
+
+    let userResult = await User.findOne({email:req.body.email}).exec();
+    console.log(req.body);
+    // console.log(userResult);
+    if(userResult != undefined) {
+
+        if (req.body.passWord == userResult.passWord) {
+            let jwt = new JwtUtil(userResult.email.toString());
+            let token = jwt.generateToken();
+            return {
+                status: 200,
+                res_msg: "Login Successfully!",
+                token: token,
+
+            };
+        } else{
+            return {
+                status:403,
+                res_msg: "Incorrect account or password!",
+            };
+        }
+    }else{
+
+        return {
+            status:404,
+            res_msg:"Account does not exist!"
+        };
+    }
+
+
+
+}
+
+module.exports={
+    signUp,
+    signIn
+}
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -6,9 +6,18 @@ const authController = require('../controllers/auth');
 
 const authRouter = express.Router();
 
-authRouter.post('/signup', async (req, res, next) => {
+authRouter.post('/signUp', async (req, res, next) => {
     try {
         res.send(await authController.signUp(req,res));
+    } catch (err) {
+        next(err);
+    }
+});
+
+authRouter.post('/signIn', async (req, res, next) => {
+    try {
+        console.log(req.body);
+        res.json(await authController.signIn(req,res));
     } catch (err) {
         next(err);
     }
